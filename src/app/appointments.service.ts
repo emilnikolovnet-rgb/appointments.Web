c783467584e70e881f39521afe6c0d9261a3a4a9
Emil Nikolov <emilnikolovnet@gmail.com>
2026-07-17 13:15:07 +0300

    Small correction to the build

--- src/app/appointments.service.ts
+++ src/app/appointments.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, catchError, map, throwError } from 'rxjs';
 import { environment } from "../environments/environment";
+import { Appointment } from "./Models/Appointment";
 
 @Injectable({
   providedIn: 'root'
